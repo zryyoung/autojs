e5702ac1b1f4f91026974268c419022b363aee52
zryyoung <1649453769@qq.com>
2023-12-09 16:28:31 +0800

    删除不必要的配置文件

--- modules/autojs-utils.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    greetingPrefix: 'Hello',
-    test() {
-        /* e.g. "Hello, AutoJs6 6.4.1" */
-        toastLog(`${this.greetingPrefix}, ${context.getString(R.strings.app_name)} ${app.autojs.versionName}`);
-    },
-};
